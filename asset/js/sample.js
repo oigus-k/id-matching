71a389d0da12c3d9af0e6598a04716cf3e42ef52
Keita SUGIO <k_sugio@yumemi.co.jp>
2014-07-04 15:43:21 +0900

    リファクタリング

--- asset/js/sample.js
+++ asset/js/sample.js
@@ -2,24 +2,10 @@
  * Created by k_sugio on 7/2/14.
  */
 
-function isProductFromNo(num, id, callback){
-    var FILE_PATH = 'asset/src/id.txt';
-    $.ajax({
-        type: 'GET',
-        url: FILE_PATH,
-        datatype: 'text',
-        success: function(data){
-            var lines = data.split('\n');
-            var result = $.inArray(String(id), lines);
-            callback(num, result, lines[result]);
-        },
-        error: function(){
-            $('#disp1').append("<p>ERROR");
-        }
-    });
-}
+var Products = typeof Products == "undefined" ? {} : Products;
 
-$(function(){
+Products.MatchID = function(){
+    var FILE_PATH = 'asset/src/id.txt';
     var ids = [
         1,
         6,
@@ -29,14 +15,37 @@ $(function(){
         10,
         13
     ];
-
-    for(var i=0;i<ids.length;i++){
-        isProductFromNo(i, ids[i], function(num, res, value){
-            if(res == -1){
-                $('#disp1').append(num + ": Unmatch<br />");
-            } else {
-                $('#disp1').append(num + ": Match => " + value + "<br />");
-            }
+    var isProductFromNo = function(num, id, callback){
+        $.ajax({
+            type: 'GET',
+            url: FILE_PATH,
+            datatype: 'text'
         })
-    }
-});
+            .done(function(data){
+                var lines = data.split('\n');
+                var result = $.inArray(String(id), lines);
+                setTimeout(function(){console.log("process 2")},3000);
+                callback(num, result, lines[result]);
+            })
+            .fail(function(){
+                $('#disp1').append("<p>ERROR");
+            });
+    };
+
+    $(function(){
+        console.log("process 1");
+        var count = ids.length;
+        for(var i=0;i<count;i++){
+            isProductFromNo(i, ids[i], function(num, res, value){
+                if(res == -1){
+                    $('#disp1').append(num + ": Unmatch<br />");
+                } else {
+                    $('#disp1').append(num + ": Match => " + value + "<br />");
+                }
+            });
+        }
+        console.log("end");
+    });
+
+};
+
